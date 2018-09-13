01f85b71759ee6b971a4c8d843214a37086e6a88
Allard Hendriksen <allard.hendriksen@cwi.nl>
2018-09-13 15:11:51 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='msd_pytorch',
-    version='0.4.0',
+    version='0.4.1',
     description="A pytorch implementation of the MSD network structure.",
     long_description=readme + '\n\n' + history,
     author="Allard Hendriksen",
