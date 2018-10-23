90da613c4d94114d0a99035a4e6c461244567d62
Allard Hendriksen <allard.hendriksen@cwi.nl>
2018-10-23 15:30:42 +0200

    Fix conv_forward on rectangular regions (v0.5.1)
    By requiring a new version of trp_convolutions, forward convolutions
    are correctly computed for rectangular regions.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='msd_pytorch',
-    version='0.5.0',
+    version='0.5.1',
     description="A pytorch implementation of the MSD network structure.",
     long_description=readme + '\n\n' + history,
     author="Allard Hendriksen",
