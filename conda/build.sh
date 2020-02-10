6a0e8e6aae2aebd330b1cdb401e33e6418d5e766
Allard Hendriksen <allard@allardhendriksen.nl>
2020-02-10 21:24:48 +0100

    gitlab-ci: Use pytorch/conda-cuda image

--- conda/build.sh
+++ conda/build.sh
@@ -8,5 +8,14 @@ env
 
 echo "------------------------------------------------------------"
 
+if [ -f $CUDA_HOME/bin/nvcc ] ; then
+    echo "-- CUDA already installed";
+else
+    echo "Using version ${cudatoolkit} "
+    export CUDA_HOME=/usr/local/cuda-${cudatoolkit}
+    echo "  set CUDA_HOME=${CUDA_HOME}"
+fi
+
+
 $PYTHON setup.py clean
 $PYTHON setup.py install --single-version-externally-managed --record record.txt || exit 1
