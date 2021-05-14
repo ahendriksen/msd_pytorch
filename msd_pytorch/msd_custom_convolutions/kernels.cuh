19044e182bb6376f2489d2000c73eccd2356d292
Allard Hendriksen <allard.hendriksen@cwi.nl>
2021-05-14 21:47:15 +0200

    Reorder kernels.cuh

--- msd_pytorch/msd_custom_convolutions/kernels.cuh
+++ msd_pytorch/msd_custom_convolutions/kernels.cuh
@@ -1,7 +1,5 @@
 #pragma once
-
 #include "device_tensor.h"
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //                         Convolution: Backward Bias                        //
@@ -19,6 +17,13 @@ conv_relu_backward_bias(dTensor4R output,
 
 template <typename scalar_t>
 __global__ void
+conv_backward_k(dTensor4R grad_output,
+                dTensor4R input,
+                dTensor4R grad_kernel,
+                int dilation);
+
+template <typename scalar_t>
+__global__ void
 conv_relu_backward_k(dTensor4R output,
                      dTensor4R grad_output,
                      dTensor4R input,
@@ -31,54 +36,22 @@ conv_relu_backward_k(dTensor4R output,
 
 template <typename scalar_t>
 __global__ void
-conv_relu_backward_x(dTensor4R output,
-                      dTensor4R grad_output,
-                      dTensor4R kernel,
-                      dTensor4R grad_input,
-		     int dilation);
-
-///////////////////////////////////////////////////////////////////////////////
-//                            Convolution:Forward                            //
-///////////////////////////////////////////////////////////////////////////////
-
-
-template <typename scalar_t>
-__global__ void
-conv_relu_forward(dTensor4R input,
-                  dTensor4R kernel,
-                  dTensor1R bias,
-                  dTensor4R output,
-                  int dilation);
-
-///////////////////////////////////////////////////////////////////////////////
-//                        Convolution: Backward Kernel                       //
-///////////////////////////////////////////////////////////////////////////////
-
-template <typename scalar_t>
-__global__ void
-conv_backward_k(dTensor4R grad_output,
-                dTensor4R input,
-                dTensor4R grad_kernel,
-                int dilation);
-
-
-///////////////////////////////////////////////////////////////////////////////
-//                           Convolution: Backward Input                     //
-///////////////////////////////////////////////////////////////////////////////
-
-template <typename scalar_t>
-__global__ void
 conv_backward_x(dTensor4R grad_output,
                 dTensor4R kernel,
                 dTensor4R grad_input,
                 int dilation);
 
-
+template <typename scalar_t>
+__global__ void
+conv_relu_backward_x(dTensor4R output,
+		     dTensor4R grad_output,
+		     dTensor4R kernel,
+		     dTensor4R grad_input,
+		     int dilation);
 
 ///////////////////////////////////////////////////////////////////////////////
 //                            Convolution:Forward                            //
 ///////////////////////////////////////////////////////////////////////////////
-
 
 template <typename scalar_t>
 __global__ void
@@ -87,3 +60,11 @@ conv_forward(dTensor4R input,
              dTensor1R bias,
              dTensor4R output,
              int dilation);
+
+template <typename scalar_t>
+__global__ void
+conv_relu_forward(dTensor4R input,
+                  dTensor4R kernel,
+                  dTensor1R bias,
+                  dTensor4R output,
+                  int dilation);
