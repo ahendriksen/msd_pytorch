25a1ebed101aacfef4c66833dee19000e23323f6
Allard Hendriksen <allard.hendriksen@cwi.nl>
2021-05-15 21:19:55 +0200

    Move 2D conv_relu into conv2d.py

--- msd_pytorch/conv2d.py
+++ msd_pytorch/conv2d.py
@@ -87,3 +87,59 @@ class Conv2dModule(nn.Module):
         return conv2d(
             input, self.weight, self.bias, self.output, self.stride, self.dilation
         )
+
+
+class Conv2dReluFunction(Function):
+    @staticmethod
+    def forward(ctx, input, weight, bias, output, stride, dilation):
+        ctx.dilation = dilation
+        conv2d_relu_forward(input, weight, bias, output.data, dilation)
+        ctx.save_for_backward(input, output, weight, bias)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # restore variables
+        input, output, weight, bias = ctx.saved_tensors
+        dilation = ctx.dilation
+
+        # ensure that grad_output is contiguous
+        grad_output = grad_output.contiguous()
+
+        # Input
+        grad_input = t.zeros_like(input)
+        conv2d_relu_backward_x(
+            output, grad_output, weight, grad_input, dilation
+        )
+        # Weight
+        grad_weight = t.zeros_like(weight)
+        conv2d_relu_backward_k(
+            output, grad_output, input, grad_weight, dilation
+        )
+        # Bias
+        grad_bias = t.zeros_like(bias)
+        conv2d_relu_backward_bias(output, grad_output, grad_bias)
+
+        return grad_input, grad_weight, grad_bias, None, None, None, None
+
+
+conv2d_relu = Conv2dReluFunction.apply
+
+
+class Conv2dReluModule(nn.Module):
+    def __init__(self, output, in_channels, out_channels, kernel_size=3, dilation=1):
+        super().__init__()
+
+        w = t.Tensor(out_channels, in_channels, kernel_size, kernel_size)
+        self.weight = Parameter(w.cuda())
+        self.bias = Parameter(t.Tensor(out_channels).cuda())
+        self.output = output
+
+        self.dilation = dilation
+        self.stride = 1
+
+    def forward(self, input):
+        assert self.output.is_cuda
+        return conv2d_relu(
+            input, self.weight, self.bias, self.output, self.stride, self.dilation
+        )
--- msd_pytorch/tests/test_conv2d.py
+++ msd_pytorch/tests/test_conv2d.py
@@ -3,22 +3,24 @@
 import pytest
 from pytest import approx
 from . import torch_equal
-import msd_pytorch.conv2d as conv
 from msd_pytorch.conv2d import (
-    # Conv2DReluModule,
-    # conv2d_relu,
     Conv2dModule,
     conv2d,
     conv2d_forward,
     conv2d_backward_x,
     conv2d_backward_k,
     conv2d_backward_bias,
+    Conv2dReluModule,
+    conv2d_relu,
+    conv2d_relu_forward,
+    conv2d_relu_backward_x,
+    conv2d_relu_backward_k,
+    conv2d_relu_backward_bias,
 )
 import torch
 from torch.autograd import Variable
 from torch.autograd import gradcheck
 import torch.nn as nn
-import msd_custom_convolutions as cc
 
 
 def test_conv2d():
@@ -46,7 +48,7 @@ def test_conv2d():
     xc = torch.ones(batch_sz, in_channels, *size).cuda()
 
     output = torch.ones(batch_sz, 1, *size).cuda()
-    ci = conv.Conv2dModule(output, in_channels, 1, kernel_size=kernel_size)
+    ci = Conv2dModule(output, in_channels, 1, kernel_size=kernel_size)
     cc = nn.Conv2d(in_channels, 1, kernel_size=kernel_size, padding=padding_torch)
 
     for c in [ci, cc]:
@@ -191,7 +193,7 @@ def test_grad_check():
         output_shape[1] = k.shape[0]
         output = x.new_zeros(output_shape, requires_grad=True)
         stride = 1
-        return conv.conv2d(x, k, b, output, stride, dilation)
+        return conv2d(x, k, b, output, stride, dilation)
 
     gradcheck(f, [x, k, b, dilation], raise_exception=True)
 
@@ -309,3 +311,188 @@ def test_conv_backward_bias():
     ref_g_bias = grad.sum((0, 2, 3))
     assert ref_g_bias.shape == g_bias.shape
     assert torch_equal(ref_g_bias, g_bias)
+
+###############################################################################
+#                           Test convolution + ReLU                           #
+###############################################################################
+
+def test_conv2d_relu():
+    """Test 2D custom convolution module
+
+    We compare our custom reflection-padded convolution to the builtin
+    Torch convolution.
+
+    We check that:
+    - The shape of the output is equal
+    - The output values are equal in the center, where the
+      reflection-padding should not have influenced the result.
+
+    """
+
+    batch_sz = 5
+    in_channels = 5
+    kernel_size = 3
+    padding_torch = 1
+    size = (45, 55)
+
+    # xi: for inplace
+    # xc: for normal torch convolution
+    xi = torch.randn(batch_sz, in_channels, *size).cuda()
+    xc = xi.data.clone()
+
+    output = torch.ones(batch_sz, 1, *size).cuda()
+    ci = Conv2dReluModule(
+        output, in_channels, 1, kernel_size=kernel_size
+    )
+    cc = nn.Sequential(
+        nn.Conv2d(in_channels, 1, kernel_size=kernel_size, padding=padding_torch),
+        nn.ReLU(),
+    )
+
+    for c in [ci, *cc.modules()]:
+        c.cuda()
+        try:
+            c.weight.data.fill_(1)
+            c.bias.data.zero_()
+        except AttributeError:
+            pass  # skip relu (it does not have parameters)
+
+    xiv = Variable(xi, requires_grad=True)
+    xcv = Variable(xc, requires_grad=True)
+
+    yi = ci(xiv)
+    yc = cc(xcv)
+    assert yi.shape == yc.shape
+
+    # Check center of output, where the output should be equal.
+    d = 1
+    yi_ = yi[:, :, d:-d, d:-d]
+    yc_ = yc[:, :, d:-d, d:-d]
+
+    # Check that pytorch and own convolution agree in the center:
+    assert torch_equal(yi_, yc_)
+    assert torch_equal(yi.data, output)
+
+
+def test_conv_relu_dtype_check():
+    """Test if dtype checks are performed correctly
+    """
+    d0 = torch.float
+    d1 = torch.double
+    dilation = 1
+
+    x = torch.zeros(1, 1, 5, 5, dtype=d0).cuda()
+    y = torch.zeros(1, 1, 5, 5, dtype=d0).cuda()
+    bias = torch.zeros(1, dtype=d1).cuda()
+    k = torch.zeros(1, 1, 3, 3, dtype=d1).cuda()
+
+    with pytest.raises(RuntimeError):
+        conv2d_relu_forward(x, k, bias, y, dilation)
+
+
+def test_conv2d_relu():
+    """Test convolution
+
+    Check if an all-zero convolution runs without runtime errors.
+    """
+    dtype = torch.float  # or t.double
+    dilation = 1
+
+    x = torch.zeros(1, 1, 5, 5, dtype=dtype).cuda()
+    y = torch.ones(1, 1, 5, 5, dtype=dtype).cuda()
+    bias = torch.zeros(1, dtype=dtype).cuda()
+    k = torch.zeros(1, 1, 3, 3, dtype=dtype).cuda()
+
+    conv2d_relu_forward(x, k, bias, y, dilation)
+    assert y.sum().item() == approx(0.0)
+
+
+def test_conv_values():
+    """Compare to pytorch convolution
+
+    Check that the convolution agrees with a pytorch convolution
+    on the output area that is not reflected.
+    """
+    dtype = torch.double
+    intensive = False
+
+    if intensive:
+        test_params = [
+            (b, c_in, c_out, dil, size)
+            for b in [1, 3, 5]
+            for c_in in [1, 3, 4]
+            for c_out in [1, 2, 3]
+            for dil in range(1, 10)
+            for size in [dil * 2 + 1, 50, 1023]
+        ]
+    else:
+        test_params = [
+            (b, c_in, c_out, dil, size)
+            for b in [2]
+            for c_in in [3]
+            for c_out in [5]
+            for dil in [1, 3, 10]
+            for size in [dil * 2 + 1, 29, 50]
+        ]
+
+    for (B, C_in, C_out, dilation, size) in test_params:
+        shape = (size, 2 * size)
+
+        # Execute my own implementation
+        x = torch.randn(B, C_in, *shape, dtype=dtype).cuda()
+        k = torch.randn(C_out, C_in, 3, 3, dtype=dtype).cuda()
+        bias = torch.randn(C_out, dtype=dtype).cuda()
+        y = torch.zeros(B, C_out, *shape, dtype=dtype).cuda()
+        conv2d_relu_forward(x, k, bias, y, dilation)
+
+        # Execute pytorch convolution:
+        conv_torch = torch.nn.Conv2d(
+            1, 1, 3, padding=dilation, dilation=dilation
+        ).cuda()
+        conv_torch.weight.data = k
+        conv_torch.bias.data = bias
+        y1 = nn.ReLU()(conv_torch(x))
+
+        assert y1.shape == y.shape
+
+        # check shapes
+        # Check center of output, where the output should be equal.
+        d = dilation
+        y_ = y[:, :, d:-d, d:-d]
+        y1_ = y1[:, :, d:-d, d:-d]
+        assert torch_equal(y1_, y_), (
+            f"for shape {shape} and dilation {dilation} "
+            f"and bias {bias}"
+            f"Your implementation:\n{y}"
+            f"\nPyTorch:\n{y1}"
+        )
+
+
+def test_conv2d_relu_grad_check():
+    """Test using gradcheck provided by Torch.
+
+    Because there is no reference implementation to compare to, we
+    check if the gradient calculations are consistent with numerical
+    calculations.
+
+    """
+    dtype = torch.double    # Extra machine precision is needed.
+    B = 2                   # Batch size
+    C_IN = 3                # Input channels
+    C_OUT = 2               # Output channels
+    H = 13                  # Height
+    W = 21                  # Width
+    dilation = 3            # Dilation
+
+    x = torch.randn(B, C_IN, H, W, dtype=dtype, requires_grad=True).cuda()
+    k = torch.randn(C_OUT, C_IN, 3, 3, dtype=dtype, requires_grad=True).cuda()
+    b = torch.randn(C_OUT, dtype=dtype, requires_grad=True).cuda()
+
+    def f(x, k, b, dilation):
+        output_shape = list(x.shape)
+        output_shape[1] = k.shape[0]
+        output = x.new_zeros(output_shape, requires_grad=True)
+        stride = 1
+        return conv2d_relu(x, k, b, output, stride, dilation)
+
+    gradcheck(f, [x, k, b, dilation], raise_exception=True)
